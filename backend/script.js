8a7a1498206aff68f9d5d4ae40f4c7b8f68007a1
yaswanth <ya8wanth@gmail.com>
2023-05-29 10:34:00 +0530

    Add Marvel project

---
+++ backend/script.js
@@ -0,0 +1,141 @@
+// Initialize and add the map
+
+//locations are fetched from my own api, start node app.js to fetch it server will run on port 3000
+
+let map;
+var positions ;
+async function initMap() {
+
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 3,
+    center: { lat: 20.613, lng: 40.209 },
+    mapId: "DEMO_MAP_ID",
+  });
+
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: positions[5],
+    title: "Soul Stone",
+  });
+  const marker2 = new AdvancedMarkerElement({
+    map: map,
+    position: positions[0],
+    title: "Time Stone",
+  });
+  const marker3 = new AdvancedMarkerElement({
+    map: map,
+    position: positions[2],
+    title: "Power Stone",
+  });
+  const marker4 = new AdvancedMarkerElement({
+    map: map,
+    position: positions[3],
+    title: "Reality Stone",
+  });
+  const marker5 = new AdvancedMarkerElement({
+    map: map,
+    position: positions[4],
+    title: "Mind Stone",
+  });
+  const marker6 = new AdvancedMarkerElement({
+    map: map,
+    position: positions[1],
+    title: "Space Stone",
+  });
+
+  const thanosMarker = new google.maps.Marker({
+    map: map,
+    position: { lat: 20.613, lng: 40.209 },
+    title: "Thanos",
+    icon: './assets/images/thanos.ico'
+  });
+  
+  function moveThanosMarkerRandomly(marker, range) {
+    const targetPoints = positions;
+  
+    const intervalId = setInterval(() => {
+      const currentPosition = marker.getPosition();
+      const latOffset = getRandomOffset(range);
+      const lngOffset = getRandomOffset(range);
+      const newLat = currentPosition.lat() + latOffset;
+      const newLng = currentPosition.lng() + lngOffset;
+      const newPosition = new google.maps.LatLng(newLat, newLng);
+      marker.setPosition(newPosition);
+  
+      for (const point of targetPoints) {
+        const distance = calculateDistance(newPosition.lat(), newPosition.lng(), point.lat, point.lng);
+        if (distance <= 1500) {
+          clearInterval(intervalId); 
+          window.alert("Avengers assemble");
+          console.log("Avengers assemble");
+          const avengerMarker = new new google.maps.Marker({
+            map: map,
+            position: {lat:point.lat,lng:point.lng},
+            title: "Avengers",
+            icon: './assets/images/avenger.png'
+          });
+          break; 
+        }
+      }
+    }, 1000);
+  }
+  
+  function calculateDistance(lat1, lng1, lat2, lng2) {
+    const radius = 6371; // Radius of the Earth in kilometers
+    const degToRad = Math.PI / 180;
+    const dLat = (lat2 - lat1) * degToRad;
+    const dLng = (lng2 - lng1) * degToRad;
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * degToRad) * Math.cos(lat2 * degToRad) *
+      Math.sin(dLng / 2) * Math.sin(dLng / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = radius * c;
+    return distance; // Distance in kilometers
+  }
+  
+  function getRandomOffset(range) {
+    return (Math.random() - 0.6) * range;
+  }
+  
+  moveThanosMarkerRandomly(thanosMarker, 50);
+  
+
+}
+fetch('http://localhost:3000/')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not OK');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); 
+    positions = data;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+initMap();
+
+// function random(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// function checkCenter() {
+//   var heart = document.getElementById('heart');
+//   var rect = heart.getBoundingClientRect();
+//   var centerX = window.innerWidth / 2;
+//   var centerY = window.innerHeight / 2;
+
+//   if (rect.left <= centerX && rect.right >= centerX && rect.top <= centerY && rect.bottom >= centerY) {
+//     console.log("Reached center");
+//   }
+// }
+
+// setInterval(checkCenter, 100);
